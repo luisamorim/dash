2139bd07a900c61f95e2dffe6e3400a5e9bf133f
kicoamorim <kicoamorim@gmail.com>
2021-10-23 18:05:35 -0300

    feat: config init thymeleaf

--- src/main/kotlin/br/com/amorim/dash/DashApplication.kt
+++ src/main/kotlin/br/com/amorim/dash/DashApplication.kt
@@ -2,10 +2,25 @@ package br.com.amorim.dash
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @SpringBootApplication
 class DashApplication
 
 fun main(args: Array<String>) {
 	runApplication<DashApplication>(*args)
+}
+
+
+@Controller
+@RequestMapping("/home")
+class HomeController{
+
+	@GetMapping()
+	fun home():String{
+		return "home"
+	}
+
 }
